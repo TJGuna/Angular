90e4a3970a8288e2d84eb327812f9fa681b17b33
TJGuna <75382994+TJGuna@users.noreply.github.com>
2024-12-17 09:57:48 -0500

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
-import {ParentComponent} from './views/databinding/parent/parent.component';
+import {HomeComponent} from "./views/home/home.component";
+import {ParentComponent} from "./views/binding/parent/parent.component";
+import {LayoutComponent} from "./layout/layout.component";
+import {SignalsComponent} from "./views/signals/signals.component";
 
 export const routes: Routes = [
-  { path: '', component: ParentComponent },
+  {
+    path:'',component:LayoutComponent,children:[
+      { path: '', component: HomeComponent },
+      { path: 'binding', component: ParentComponent },
+      { path: 'signals', component: SignalsComponent },
+    ]},
   // { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  // Add more routes as needed
 ];
